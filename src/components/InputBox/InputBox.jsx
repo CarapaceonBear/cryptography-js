47e21562bf2cd2ab35e073b21b96e14006beea1d
carapaceonbear <seerhan17@gmail.com>
2022-06-17 09:57:21 +0100

    component structure

---
+++ src/components/InputBox/InputBox.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./InputBox.scss";
+
+
+const InputBox = () => {
+  return (
+    <div>InputBox</div>
+  )
+}
+
+export default InputBox
---
+++ src/containers/Main/Main.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Main.scss";
+import Button from "../../components/Button/Button";
+import DropDown from "../../components/DropDown/DropDown";
+import InputBox from "../../components/InputBox/InputBox";
+
+const Main = () => {
+  return (
+    <div>Main</div>
+  )
+}
+
+export default Main
